f245b2a41295732cf8fdba49f44ba0f2d2a2d769
birawaich <27787788+birawaich@users.noreply.github.com>
2025-07-29 11:53:53 +0200

    support structure of builddescriptor file

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ DocumentColllection.verbose = True
 SingleDocument.VERBOSE = True
 
 # Settings
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/ATIC__advanced-topics-in-control/build.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/ATIC__advanced-topics-in-control/build_lectures.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/NSC__nonlinear-systems-and-control/build_lectures.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/NSC__nonlinear-systems-and-control/build.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/NSC__nonlinear-systems-and-control/build_lyapunov-theory.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/MPC__model-predictive-control/build_lectures.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/MPC__model-predictive-control/build.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/PSDC__power-systems-dynamics-and-control/build_lectures.yaml'
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/REE__recursive-estimation/build_lectures.yaml' 
-# builddescriptor_path = '/home/bstadler/polybox/ZETTLR_STUDIES/REE__recursive-estimation/build.yaml' 
 builddescriptor_path = 'sample/build.yaml'
 
 # SETUP
